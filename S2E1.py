334177afd7e8c39c1a2eabe4b5ea0240d5bda380
Enzo Koshimura <59707759+ekshmr@users.noreply.github.com>
2022-10-05 20:16:07 -0300

    Atualização S2E1

--- S2E1.py
+++ S2E1.py
@@ -1,5 +1,5 @@
 calcular_horas_trabalhadas_mes: bool
-resposta: str = str(input("Deseja saber quantas horas você trabalha ao mês? (S/n)"))
+resposta: str = str(input("Deseja saber quantas horas você trabalha ao mês? (S/n) "))
 match resposta:
     case "S":
         calcular_horas_trabalhadas_mes = True
@@ -7,7 +7,7 @@ match resposta:
         calcular_horas_trabalhadas_mes = True
     case "N":
         calcular_horas_trabalhadas_mes = False
-    case "s":
+    case "n":
         calcular_horas_trabalhadas_mes = False
 
 match calcular_horas_trabalhadas_mes:
@@ -16,9 +16,11 @@ match calcular_horas_trabalhadas_mes:
         horas_trabalhadas_mes: float = horas_trabalhadas_semana * 4
     case False:
         horas_trabalhadas_mes: float = float(input("Quantas horas você trabalha por mês? "))
+        horas_trabalhadas_semana: float = horas_trabalhadas_mes / 4
+
 
 calcular_valor_por_hora: bool
-resposta = str(input("Deseja saber o valor de horas trabalhadas ao mês? (S/n)"))
+resposta = str(input("Deseja saber o valor de horas trabalhadas ao mês? (S/n) "))
 match resposta:
     case "S":
         calcular_valor_por_hora = True
@@ -31,12 +33,29 @@ match resposta:
 
 match calcular_valor_por_hora:
     case True:
-        valor_salario: float = float(input("Quanto você ganha por mês? "))
-        valor_por_hora: float = valor_salario / horas_trabalhadas_mes
+        valor_salario_bruto: float = float(input("Quanto você ganha por mês? "))
+        valor_por_hora: float = round(valor_salario_bruto / horas_trabalhadas_mes, 2)
     case False:
         valor_por_hora: float = float(input("Quanto você ganha por hora? "))
-        valor_salario: float = valor_por_hora * horas_trabalhadas_mes
+        valor_salario_bruto: float = valor_por_hora * horas_trabalhadas_mes
 
+def valor_imposto_renda(valor: float) -> float:
+    return valor * 0.08
 
+def valor_inss(valor: float) -> float:
+    return valor * 0.11
 
-print(f"{valor_por_hora}\n{horas_trabalhadas_mes}")
+def valor_sindicato(valor: float) -> float:
+    return valor * 0.05
+
+imposto_renda: float = valor_inss(valor_salario_bruto)
+inss: float = valor_inss(valor_salario_bruto)
+sindicato: float = valor_sindicato(valor_salario_bruto)
+valor_salario_total: float = (valor_salario_bruto) - (imposto_renda + inss + sindicato)
+
+print(f"\nHoras trabalhadas na semana: {horas_trabalhadas_semana}\nHoras trabalhadas ao mês {horas_trabalhadas_mes}")
+print(f"Valor Bruto do salário: {valor_salario_bruto}\nValor por hora: {valor_por_hora}")
+
+print(f"Imposto de renda: {imposto_renda}\nINSS: {inss}\nSindicato: {sindicato}")
+
+print(f"Valor Total do salário: {valor_salario_total}")
