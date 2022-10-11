6b6bb9af7a79dfd1a2dd9654e37207352b1b926d
Enzo Koshimura <59707759+ekshmr@users.noreply.github.com>
2022-10-10 21:01:57 -0300

    feat: s4e2 type checking meio cagado

--- S4E2.py
+++ S4E2.py
@@ -1,22 +1,11 @@
-'''
-2) Construa um programa em PYTHON utilizando uma pilha que resolva o 
-seguinte problema:
-Armazene as placas dos carros que estão estacionados numa rua sem saída 
-estreita. Dado uma placa verifique se o carro está estacionado na rua. Caso 
-esteja, informe a sequência de carros que deverá ser retirada para que o carro 
-em questão consiga sair.
-'''
-
-import random
-
 class Pilha:
     def __init__(self):
-        self.itens: list = []
+        self.itens: list[str] = []
 
     def vazia(self):
         return self.itens == []
 
-    def inserir(self, item: int):
+    def inserir(self, item: str):
         self.itens.append(item)
 
     def remover(self):
@@ -31,10 +20,10 @@ class Pilha:
 cabe_carro: bool = True
 quantidade_carros: int = 0
 alfabeto_maiusculas: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-id_carro: int
-contador: int
-placa_generica: int = "AAA0A00"
-placa_carro: int
+id_carro: int = int()
+contador: int = int()
+placa_generica: str = "AAA0A00"
+placa_carro: list[int] = list()
 caractere: str
 
 while cabe_carro == True:
@@ -43,4 +32,4 @@ while cabe_carro == True:
     # AAA0A00
     for caractere in placa_generica:
         contador += 1
-        placa_carro[id_carro][contador]
+        placa_carro[id_carro]
